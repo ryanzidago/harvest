1def5d0bfb1929fd06dcf7e532b4dbda156f1679
Ryan Zidago <ryanzidago@gmail.com>
2021-01-14 23:44:47 +0100

    used database name instead of database url

---
+++ release.sh
@@ -0,0 +1,4 @@
+cargo build \
+        --release \
+        --verbose \
+&& cp target/release/harvest ./bin
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 use std::env;
-use std::process::Command;
 use std::process::exit;
+use std::process::Command;
 
 fn main() {
     let argv: Vec<String> = env::args().collect();
 
     maybe_display_help(&argv[1]);
-    maybe_display_error_message();
 
     let app_name: String = parse_arg(&argv[1], &String::from("--app="));
     let env_name: String = parse_arg(&argv[2], &String::from("--env="));
     let local_database_name: String = parse_arg(&argv[3], &String::from("--local-db="));
 
-    let app_name_with_env: String = build_app_name_with_env(&app_name, &env_name);
-    let database_url: String = get_database_url(&app_name_with_env);
-
-    set_database_url_env_var(database_url);
-    pull_from_remote_database(&app_name_with_env, &local_database_name);
-    unset_database_url_env_var();
+    let heroku_app_name: String = build_heroku_app_name(&app_name, &env_name);
+    let remote_database_name = get_remote_database_name(&heroku_app_name);
+    pull_from_remote_database(
+        &heroku_app_name,
+        &remote_database_name,
+        &local_database_name,
+    );
 }
 
 fn maybe_display_help(arg: &String) {
@@ -27,7 +27,7 @@ Small command line program to pull data from a database hosted on Heroku.
 Requires the heroku CLI to be installed.
 
 USAGE:
-    harvest [args]
+    harharget_database_namevestget_database_namevest [args]
 
 ARGS:
     --app=<app>
@@ -47,60 +47,44 @@ EXAMPLE:
     }
 }
 
-fn maybe_display_error_message() {
-    let key = "DATABASE_URL";
-    match env::var(key) {
-        Ok(val) => if !val.is_empty() {
-            eprintln!("DATABASE_URL is already set.\nUnset DATABASE_URL environment variable before continuing.\n(hint: run `unset DATABASE_URL`)");
-            exit(-1);
-        }
-        Err(e) => {
-            eprintln!("Couldn't interpret {}: {}", key, e);
-            exit(-1);
-        }
-    }
-}
-
 fn parse_arg(arg: &String, pattern: &String) -> String {
     arg.replace(pattern, "")
 }
 
-fn build_app_name_with_env<'a>(app_name: &'a String, env_name: &'a String) -> String {
+fn build_heroku_app_name<'a>(app_name: &'a String, env_name: &'a String) -> String {
     format!("{}-{}", app_name, env_name)
 }
 
-fn get_database_url(app_name_with_env: &String) -> String {
+fn get_remote_database_name(heroku_app_name: &String) -> String {
     let output = Command::new("heroku")
-                    .arg("config:get")
-                    .arg("DATABASE_URL")
-                    .arg("--app")
-                    .arg(app_name_with_env)
-                    .output()
-                    .expect("Failed to execute command.");
+        .arg("pg:info")
+        .arg("--app")
+        .arg(heroku_app_name)
+        .output()
+        .unwrap()
+        .stdout;
 
-    let mut database_url: String = String::from_utf8(output.stdout).unwrap();
-    database_url.pop();
-    database_url
+    let output = String::from_utf8(output).unwrap();
+
+    output.rsplit(" ")
+        .collect::<Vec<&str>>()[0]
+        .trim_end()
+        .to_string()
 }
 
-fn set_database_url_env_var(database_url: String) {
-    let key = "DATABASE_URL";
-    env::set_var(key, database_url);
-}
-
-fn pull_from_remote_database<'a>(app_name_with_env: &'a String, local_database_name: &'a String) {
+fn pull_from_remote_database<'a>(
+    heroku_app_name: &'a String,
+    remote_database_name: &'a String,
+    local_database_name: &'a String,
+) {
     let output = Command::new("heroku")
-                    .arg("pg:pull")
-                    .arg("DATABASE_URL")
-                    .arg(local_database_name)
-                    .arg("--app")
-                    .arg(app_name_with_env)
-                    .output()
-                    .expect("Failed to execute command.");
+        .arg("pg:pull")
+        .arg(remote_database_name)
+        .arg(local_database_name)
+        .arg("--app")
+        .arg(heroku_app_name)
+        .output()
+        .expect("Failed to execute command.");
 
     println!("{}", String::from_utf8(output.stdout).unwrap());
-}
-
-fn unset_database_url_env_var() {
-    env::remove_var("DATABASE_URL");
 }
